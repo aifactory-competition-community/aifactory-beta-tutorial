422ea91a553762f9006e519a1e8b0e6589f403cc
yoosunyoung <luysunyoung9@gmail.com>
2021-10-30 19:00:14 +0900

    tutorial with lottery task

---
+++ scripts/auto_lottery.py
@@ -0,0 +1,9 @@
+from random import shuffle
+
+
+if __name__ == "__main__":
+    number_set = list(range(1, 46))
+    with open("sample_data/answer.csv", 'w') as file:
+        for _ in range(6):
+            shuffle(number_set)
+            file.write(str(number_set.pop())+"\n")
